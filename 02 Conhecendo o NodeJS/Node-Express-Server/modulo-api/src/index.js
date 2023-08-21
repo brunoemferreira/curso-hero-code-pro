f89baa08be4b7d9b487af008c6180b97084c138e
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2023-08-21 15:07:34 -0300

    Criaçao do primeiro servidor nodeJs com Express

---
+++ 02 Conhecendo o NodeJS/Node-Express-Server/modulo-api/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    console.log(request.method);
+
+    response.status(200).json({
+        message: 'Hello World!!!',
+    })
+})
+
+app.listen(3333, () => {
+    console.log('Server is Active !!!');
+})
